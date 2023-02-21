bb42040d89b106df59b66d058a66373ffb3dec50
“Dubem <dubemobinnaesiowu@gmail.com>
2023-02-21 10:34:58 -0800

    3-islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _islower - Check Lowercase Character
+ * @c: The Character To Be Checked
+ * Return: 1 For Lowercase Or 0 For Other
+ */
+
+int _islower(int c)
+{
+	if(c >= 97 && c <= 122)
+	{
+
+	return (1);
+	}
+	return (0);
+}
+
+
+
