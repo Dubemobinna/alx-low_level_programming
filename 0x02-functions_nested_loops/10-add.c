26766e994369fd00d2202a32d4e27ff455ca69e6
“Dubem <dubemobinnaesiowu@gmail.com>
2023-02-21 13:20:32 -0800

    10-add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * add - Adds Two Values
+ * @n1: The First Number To Be Computed
+ * @n2: The Second Number To Be Computer
+ * Return: Integer Value
+ */
+
+int add(int n1, int n2)
+{
+	return (n1 + n2);
+}
+
