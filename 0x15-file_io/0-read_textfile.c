9ec735f9b1c9f8611cc828b57da2453ca90af04c
“Dubem <dubemobinnaesiowu@gmail.com>
2023-04-09 03:32:17 -0700

    Compiled 0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,5 +1,5 @@
 #include "main.h"
-#include <stdlib>
+#include <stdlib.h>
 
 /**
  * read_textfiile - this function reads a textfile and prints to STDOUT
@@ -15,23 +15,23 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	ssize_t x, y, w;
 
 	x = open(filename, O_RDONLY);
-	y = read(x, buffer, letters);
-	z = write(STDOUT_FILENO, buffer, y);
-
 	if (filename == NULL)
 		return (0);
 
-	buf = malloc(sizeof(char) * letters);
-	if (buf == NULL)
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
 		return (0);
 
-	if (o == -1 || r == -1 || w == -1 || w != r)
+	y = read(x, buffer, letters);
+	w = write(STDOUT_FILENO, buffer, y);
+
+	if (x == -1 || y == -1 || w == -1 || w != y)
 	{
-		free(buf);
+		free(buffer);
 		return (0);
 	}
 
-	free(buf);
+	free(buffer);
 	close(x);
 
 	return (w);
